9f89faa4861fee94d0f4872acc2d3a3abe7b66c1
Matheus Rodrigues <matheusrod92@gmail.com>
2025-10-08 00:48:14 -0300

    add vercel deploy

--- src/api-docs/openAPIRouter.ts
+++ src/api-docs/openAPIRouter.ts
@@ -6,9 +6,20 @@ import { generateOpenAPIDocument } from "@/api-docs/openAPIDocumentGenerator";
 export const openAPIRouter: Router = express.Router();
 const openAPIDocument = generateOpenAPIDocument();
 
+const swaggerUiOptions = {
+	explorer: true,
+	customCss:
+		".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+	customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css",
+};
+
+const swaggerUiDistPath = require("swagger-ui-dist").getAbsoluteFSPath();
+
 openAPIRouter.get("/swagger.json", (_req: Request, res: Response) => {
 	res.setHeader("Content-Type", "application/json");
 	res.send(openAPIDocument);
 });
 
-openAPIRouter.use("/", swaggerUi.serve, swaggerUi.setup(openAPIDocument));
+openAPIRouter.use("/", express.static(swaggerUiDistPath));
+
+openAPIRouter.use("/", swaggerUi.serve, swaggerUi.setup(openAPIDocument, swaggerUiOptions));
